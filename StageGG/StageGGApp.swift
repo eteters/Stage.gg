5a4f3f84d530a6740c79366645b1b31164560412
eteters <evan@teters.net>
2021-04-09 18:22:50 -0500

    Initial commit and gitignore

---
+++ StageGG/StageGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageGGApp.swift
+//  StageGG
+//
+//  Created by Evan Teters on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct StageGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
